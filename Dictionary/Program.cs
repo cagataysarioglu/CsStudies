de945fb78947bc319c89162df2f3b3f610a4344d
cagataysarioglu <sarioglucagatay@gmail.com>
2021-01-24 10:56:09 +0300

    Dictionary added.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(552, "The establishment of the khanate.");
+            dictionary.Add(639, "The Kursad's revolution.");
+            dictionary.Add(692, "The period of the Kapgan.");
+
+            foreach (KeyValuePair<int, string> item in dictionary)
+            {
+                Console.WriteLine("{0} and {1}", item.Key, item.Value);
+            }
+
+            Console.WriteLine();
+
+            dictionary.Remove(552);
+
+            foreach (KeyValuePair<int, string> item in dictionary)
+            {
+                Console.WriteLine("{0} and {1}", item.Key, item.Value);
+            }
+            Console.WriteLine();
+
+            dictionary.Clear();
+
+            Console.WriteLine("The dictionary includes {0} key/value.", dictionary.Count);
+
+        }
+    }
+}
--- OOPOne/Program.cs
+++ OOPOne/Program.cs
@@ -6,7 +6,25 @@ namespace OOPOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IndividualCustomer customerOne = new IndividualCustomer();
+            customerOne.Id = 1;
+            customerOne.TRId = "8877993311";
+            customerOne.CustomerNo = "34567";
+            customerOne.Name = "Çağatay";
+            customerOne.Surname = "Sarıoğlu";
+
+            CorporateCustomer customerTwo = new CorporateCustomer();
+            customerTwo.Id = 2;
+            customerTwo.CustomerNo = "987345";
+            customerTwo.TaxNo = "738495";
+            customerTwo.FirmName = "hçsYazılım";
+
+            Customer customerThree = new IndividualCustomer();
+            Customer customerFour = new CorporateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customerOne);
+            customerManager.Add(customerThree);
         }
     }
 }
