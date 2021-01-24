7fb47e2323c73ea689adc263afd2d43cc83f2b1d
cagataysarioglu <sarioglucagatay@gmail.com>
2021-01-24 15:21:24 +0300

    Updated.

--- VoucherSystem/Program.cs
+++ VoucherSystem/Program.cs
@@ -6,8 +6,22 @@ namespace VoucherSystem
     {
         static void Main(string[] args)
         {
+            GameSeller gameSeller = new GameSeller
+            {
+                Id = 40, TaxpayerNo = "3758906", BirthYear = 1990, Name = "Sencer", Surname = "Kapganoğlu"
+            };
+
             Player playerOne = new Player();
             playerOne.Register();
+            playerOne.Update();
+        }
+    }
+
+    class SalesManager
+    {
+        static void Sell(IPlayerService player)
+        {
+            Console.WriteLine(player + "için satış yapıldı.");
         }
     }
 }
