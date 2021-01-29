bac9ccc167d7dabd0e4f79b04dd798369e3a2228
cagataysarioglu <sarioglucagatay@gmail.com>
2021-01-29 14:23:41 +0300

    An sql example added.

---
+++ SQLConnection/Connector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLConnection
+{
+    class Connector
+    {
+        public void Perform(string query)
+        {
+            Console.WriteLine("The query has been performed.");
+        }
+    }
+}
---
+++ SQLConnection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQLConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string queryString = "SELECT Products.ProductName AS ProductName, SUM([Order Details].UnitPrice * [Order Details].Quantity) AS TotalGain" +
+                                 "FROM Products" +
+                                 "INNER JOIN [Order Details] ON Products.ProductID = [Order Details].ProductID" +
+                                 "INNER JOIN Orders ON Orders.OrderID = [Order Details].OrderID" +
+                                 "GROUP BY Products.ProductName" +
+                                 "ORDER BY ProductName, TotalGain";
+            Connector connector = new Connector();
+            connector.Perform(queryString);
+        }
+    }
+}
