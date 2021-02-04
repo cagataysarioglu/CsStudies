9b3134c15d4010da8b554dbf3ff1856f5e5b9956
cagataysarioglu <sarioglucagatay@gmail.com>
2021-02-04 17:28:41 +0300

    Database sets renamed.

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -12,8 +12,8 @@ namespace DataAccess.Concrete.EntityFramework
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=Northwind; Trusted_Connection=true");
         }
-        public DbSet<Car> Products { get; set; }
-        public DbSet<Brand> Categories { get; set; }
-        public DbSet<Color> Customers { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
     }
 }
